15aaa3f2c2973e5ce6b51376756ad667b33ae36f
Nevin <nevin32007@gmail.com>
2026-09-11 01:02:06 +0530

    Revert "Improved the Modularization of buildings and scene changes"
    This reverts commit 034d0a8f3852a273cc598663e35f1d9240cdb805.

--- Interact.gd
+++ Interact.gd
@@ -2,14 +2,14 @@ extends Node2D
 
 
 
-func _on_academic_building_body_entered(body : Player):
+func _on_academic_building_body_entered(body):
 	print("area enterd")
 	
-	body.show_interact_labeal()
+	body.show_interact_label()
 	pass # Replace with function body.
 
 
-func _on_academic_building_body_exited(body : Player):
+func _on_academic_building_body_exited(body):
 	print("Area exited")
 	
 	body.hide_interact_label()
--- player/script/player.gd
+++ player/script/player.gd
@@ -1,6 +1,4 @@
 extends CharacterBody2D
-class_name Player
-
 @onready var anim = $AnimatedSprite2D
 @onready var interact_label: Label = $Label
 @onready var start_ui = $"../StartUI"
