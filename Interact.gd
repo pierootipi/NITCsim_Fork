d175c88c32a1e65e4ae9b707291b93c52d473ca4
Nevin <nevin32007@gmail.com>
2026-09-11 01:15:32 +0530

    Improved modularity of buildings in main map , made inside.tscn exitable

--- Interact.gd
+++ Interact.gd
@@ -3,9 +3,9 @@ extends Node2D
 
 
 func _on_academic_building_body_entered(body : Player):
-	print("area enterd")
+	print("Area entered")
 	
-	body.show_interact_labeal()
+	body.show_interact_label()
 	pass # Replace with function body.
 
 
